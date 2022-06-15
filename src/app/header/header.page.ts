3ffd475a37c4e5314158a06b9bbe02311e7aecd7
Leandrowebdeveloper <leandro.web.developer.10@gmail.com>
2022-06-15 20:40:10 -0300

    Criando interface grafica no painel de controle

--- src/app/header/header.page.ts
+++ src/app/header/header.page.ts
@@ -1,5 +1,15 @@
+import { tap } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user/user.service';
+import { User } from 'src/app/interface/index';
+import { Observable, of, Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnDestroy,
+} from '@angular/core';
 import { Platform, PopoverController } from '@ionic/angular';
 import { MenuComponent } from './menu/menu.component';
 
@@ -13,18 +23,73 @@ import { MenuComponent } from './menu/menu.component';
  * @class HeaderPage
  * @implements OnInit
  */
-export class HeaderPage implements OnInit {
-  private _title: string;
+export class HeaderPage implements OnInit, OnDestroy {
+  @Output() user = new EventEmitter<User>(undefined);
   public hasIos: boolean;
+  public avatar: string = './../../assets/avatar.svg';
+  public name: string;
+  public userRouterPage: string[] = ['/painel-de-controle', 'usuarios'];
+  private _user$: Observable<User>;
+
+  private _title: string;
+  private $user: Subscription;
 
   constructor(
     private plt: Platform,
     private popoverController: PopoverController,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
   ) {}
+
   ngOnInit() {
+    this.init();
     this.title = this.activatedRoute.snapshot.data.breadcrumb;
     this.isPlatformIos();
+  }
+
+  ngOnDestroy(): void {
+    this.$user.unsubscribe();
+  }
+
+  private init() {
+    this.setUser();
+    this.getUser();
+  }
+
+  private get user$(): Observable<User> {
+    return this._user$;
+  }
+
+  private set user$(value: Observable<User>) {
+    this._user$ = value;
+  }
+
+  private setUser() {
+    return (this.$user = this.userService.userObservable.subscribe(
+      (user: User) => {
+        this.user$ = of(user);
+        this.getAvatar(user);
+        this.getName(user);
+      }
+    ));
+  }
+
+  private getAvatar(user: User): void {
+    if (user.image.url) {
+      this.avatar = user.image.url;
+    }
+  }
+
+  private getName(user: User): void {
+    if (user.firstName && user.lastName) {
+      this.name = `${user.firstName} ${user.lastName}`;
+    }
+  }
+
+  private getUser() {
+    return (this.$user = this.user$.subscribe((user: User) =>
+      this.user.emit(user)
+    ));
   }
 
   private isPlatformIos(): boolean {
--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/app/interface/index';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
+  public readonly nav = [
+    {
+      name: 'Usuários',
+      title: 'Manutenção do usuário.',
+      router: ['/painel-de-controle', 'usuarios'],
+      icon: 'person',
+      label: 'Página do usuário'
+    },
+    {
+      name: 'Anúncios',
+      title: 'Manutenção do anúncio.',
+      router: ['/painel-de-controle', 'anuncios'],
+      icon: 'newspaper',
+      label: 'Página de anúncio'
+    },
+    {
+      name: 'Extatísticas',
+      title: 'Manutenção do anúncios.',
+      router: ['/painel-de-controle', 'extatistica'],
+      icon: 'analytics',
+      label: 'Página de extatística'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public setUser(user: User){
+    // console.log(user);
+  }
+
+
 
 }
